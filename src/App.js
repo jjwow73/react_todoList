cc0735475ea8185fee7db49a61d44f4f1cf9a8cd
jjwow73 <jongwow73@gmail.com>
2019-06-09 10:53:59 +0900

    functional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useRef, useState, useCallback } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
 import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
@@ -6,96 +6,69 @@ import Palette from './components/Palette';
 
 const colors = ['#343a40', '#f03e3e', '#12b886', '#228ae6'];
 
-class App extends Component {
-  id = 3;
-  state = {
-    input: '',
-    todos: [
-      {id: 0, text: ' 리액트 소개0', checked: false },
-      {id: 1, text: ' 리액트 소개1', checked: true },
-      {id: 2, text: ' 리액트 소개2', checked: false },
-    ],
-    color:'#343a40',
-  }
-  handleChange = (e) => {
-    this.setState({
-      input: e.target.value
-    });
-  }
-  handleCreate = () => {
-    const { input, color, todos } = this.state;
-    this.setState({
-      input: '',
-      todos: todos.concat({
-        id: this.id++,
+const App = () => {
+  const id = useRef(3);
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([
+    {id: 0, text: ' 리액트 소개0', checked: false },
+    {id: 1, text: ' 리액트 소개1', checked: true },
+    {id: 2, text: ' 리액트 소개2', checked: false },
+  ]);
+  const [color, setColor] = useState('#343a40');
+  
+  const handleChange = useCallback((e)=>{
+    setInput(e.target.value);
+  });
+  const handleCreate = useCallback(()=>{
+    setTodos(
+      todos.concat({
+        id: id.current++,
         text: input,
         checked: false,
         color: color,
       })
-    });
-  }
-  handleKeyPress = (e) => {
-    //눌려진 키가 Enter면 handleCreate 호출
-    if(e.key === 'Enter'){
-      this.handleCreate();
-    }
-  }
-  handleToggle = (id) => {
-    const { todos } = this.state;
-    //파라미터로 받은 id를 가지고 몇 번째 아이템인지 찾습니다.
+    );
+    setInput('');
+  },[input, todos]);
+  const handleKeyPress = useCallback((e) => {
+    if(e.key === 'Enter')
+      handleCreate();
+  },[]);
+  const handleToggle = useCallback((id)=> {
     const index = todos.findIndex(todo => todo.id === id);
     const selected = todos[index];
     const nextTodos = [...todos];
-
     nextTodos[index] = {
       ...selected,
       checked: !selected.checked
     };
-
-    this.setState({
-      todos: nextTodos
-    });
-  }
-  handleRemove = (id) => {
-    const { todos } = this.state;
-    this.setState({
-      todos: todos.filter(todo => todo.id !== id)
-    });
-  }
-  handleColorChange = (color) => {
+    setTodos(nextTodos);
+  },[todos]);
+  const handleRemove = useCallback((id) => {
+      setTodos(todos.filter(todo => todo.id !== id));
+  },[todos]);
+  const handleColorChange = useCallback((color) => {
     console.log(color);
-    this.setState({
-      color:color
-    })
-  }
-  render() {
-    const { input, todos, color } = this.state;
-    const {
-      handleChange,
-      handleCreate,
-      handleKeyPress,
-      handleToggle,
-      handleRemove,
-      handleColorChange
-    } = this;
-    return (
-      <TodoListTemplate 
-        form={(<Form
-          value={input}
-          onKeyPress={handleKeyPress}
-          onChange={handleChange}
-          onCreate={handleCreate}
-          color={color}
-        />)}
-        palette={(<Palette
-          colors={colors}  
-          selected={color}
-          onSelect={handleColorChange}
-        />)}
-      >
-        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove} />
-      </TodoListTemplate>
-    );
-  }
+    setColor(color);
+  },[color]);
+  return (
+    <TodoListTemplate 
+      form={(<Form
+        value={input}
+        onKeyPress={handleKeyPress}
+        onChange={handleChange}
+        onCreate={handleCreate}
+        color={color}
+      />)}
+      palette={(<Palette
+        colors={colors}  
+        selected={color}
+        onSelect={handleColorChange}
+      />)}
+    >
+      <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove} />
+    </TodoListTemplate>
+  
+  );
 }
 export default App;
